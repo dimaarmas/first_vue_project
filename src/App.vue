49148733908f03e679079e9ea1ee176b42e52c17
Dumitru Armas <dimaarmas@Dumitrus-MacBook-Air.local>
2024-02-19 16:50:38 +0100

    implemented event handling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
-<script>
-
-</script>
-
 <template>
-  <h1>Hello!</h1>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+
+  <button type="button" @mouseover ='userData()'>Click me!</button>
 </template>
 
-<style scoped>
+<script>
+  export default{
+     data(){
+      return{
+        info: 'Some Title',
+        someInfo: 'Some new text'
+      }
+    },
 
+    methods: {
+      userData (){
+        this.info = "new title",
+        this.someInfo = "new text"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+h3 {
+  font-weight: lighter;
+}
+
+p{
+  color: rgb(203, 31, 31)
+}
 </style>
